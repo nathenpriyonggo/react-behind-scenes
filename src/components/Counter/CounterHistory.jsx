a26f3831170c0188c668b4e7bed179a67ac8e0b9
nathen-win <rpriyo.uic.edu>
2025-03-27 00:44:34 -0500

    Fixed indexing issue in CounterHistory

--- src/components/Counter/CounterHistory.jsx
+++ src/components/Counter/CounterHistory.jsx
@@ -19,12 +19,12 @@ function HistoryItem({ count }) {
 }
 
 export default function CounterHistory({ history }) {
-  log('<CounterHistory /> rendered', 2);
+  log('<CounterHistory /> rendered', 2); 
 
   return (
     <ol>
-      {history.map((count, index) => (
-        <HistoryItem key={index} count={count} />
+      {history.map((count) => (
+        <HistoryItem key={count.id} count={count.value} />
       ))}
     </ol>
   );
